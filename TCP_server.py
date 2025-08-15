600f608523f0a98df1a79f72ff068b88c2381116
TheJhyeFactor <aeo.dev00001@gmail.com>
2025-08-16 01:24:00 +1000

    fixed some types and had input type the wrong way around lol mindnight coding things haha

--- TCP_server.py
+++ TCP_server.py
@@ -1,8 +1,26 @@
-import stocket
+import socket
 
 
 tcp_soc = socket.socket()
 print("Socket Created: No Issues")
 
 
-port = input(int(f"Please enter the socket you want to listen on"))
+port_input = int(input(f"Please enter the socket you want to listen on: "))
+
+
+tcp_soc.bind(('', port))
+print("Sokcet binded to %s" %(port))
+
+tcp_soc.listen(5)
+print("Socket is listening")
+
+
+while True:
+    c, addr  = tcp_soc.accept()
+    print('Got connection from ', addr)
+    
+    c.send('Thanking you for connecting' .encode())
+    
+    c.close
+    
+    break
