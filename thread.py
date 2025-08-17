2a145ea63142b6a8a3a63c9ec7491b731c87bd9c
TheJhyeFactor <aeo.dev00001@gmail.com>
2025-08-17 16:12:17 +1000

    Just playing around with multi inputs & spliting an arary but starting and max index i also was playing around reading the max len and using that but it was a but redundant

--- thread.py
+++ thread.py
@@ -2,16 +2,20 @@ import threading
 
 ips_bby = ['192', '827', '2726']
 
-def fucn(ip_lis):
+ports = ["10", "90" ,"80", "80"]
+
+def fucn(ip_lis, port):
     for ip in ip_lis:
-        threading.Thread(target=prints, args=(ip, )).start()
+        threading.Thread(target=prints, args=(ip, port,)).start()
         print("called")
         
       
-def prints(ips):
+def prints(ips, portt):
+    ports = portt
+    port_lenmax = len(portt)
     item = ips
-    print(item)  
+    print(f"{item} \n {ports[:2]} HELLOOOO")  
       
       
         
-fucn(ips_bby)
+fucn(ips_bby, ports)
